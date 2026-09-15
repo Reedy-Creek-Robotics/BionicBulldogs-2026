6ff6d7c3f3c0faaf3ba54e43cd55d41b15373d80
catcat-10000 <luke.mckay05@gmail.com>
2026-09-15 15:42:30 -0400

    Created an auto path. It compiles but it is unknown if it works

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/TestAutoLeft.java
@@ -0,0 +1,26 @@
+package org.firstinspires.ftc.teamcode.opmodes;
+
+
+import com.bylazar.configurables.annotations.Configurable;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.paths.Path;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import org.firstinspires.ftc.teamcode.pedroPathing.AutoPaths;
+
+@Configurable
+@TeleOp(name = "AutoTestLeft", group = "Pedro Pathing")
+class TestAutoLeft extends AutoPaths {
+    @Override
+    public void init(){
+        follower.setStartingPose(point1A);
+        follower.update();
+        drawCurrent();
+        path2_5 = follower.pathBuilder()
+                .addPath(new BezierLine(point2, point3))
+                .setLinearHeadingInterpolation(point2.getHeading(), point3.getHeading())
+                .addPath(new BezierLine(point3, point3_1))
+                .setLinearHeadingInterpolation(point3.getHeading(),point3_1.getHeading())
+                .build();
+        startPath = new Path(new BezierLine(leftStart, point2));
+    }
+}
